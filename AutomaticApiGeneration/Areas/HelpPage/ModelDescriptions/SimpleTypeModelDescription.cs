5cbd04cd3a03ff7c61c64232a7d8935cf264ef55
arnargisla <arnargisla@gmail.com>
2019-02-07 02:03:14 +0100

    Add example api client generation
    An ASP.NET API project with automatic typescript client generation on
    build via Swashbuckle and NSwag

---
+++ AutomaticApiGeneration/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AutomaticApiGeneration.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ AutomaticApiGeneration/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AutomaticApiGeneration.Controllers
+{
+    public class TestController : ApiController
+    {
+        // GET api/test
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "test", "test2" };
+        }
+        // GET api/test/1
+        public IEnumerable<string> Get(int id)
+        {
+            return new string[] { "test" };
+        }
+    }
+}
